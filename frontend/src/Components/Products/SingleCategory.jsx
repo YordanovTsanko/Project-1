96fe7514b4c534c701b1444b7ed74e8aad5121f6
Shad0wPaw <faxyhitz1@gmail.com>
2024-07-15 18:44:54 +0300

    styled

--- frontend/src/Components/Products/SingleCategory.jsx
+++ frontend/src/Components/Products/SingleCategory.jsx
@@ -106,6 +106,7 @@ const SingleCategory = () => {
                   display: "grid",
                   gridTemplateColumns: {
                     xs: "repeat(1, 1fr)",
+                    sm: "repeat(2, 1fr)",
                     md: "repeat(3, 1fr)",
                     lg: "repeat(4, 1fr)",
                   },
